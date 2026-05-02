189b3dfa9ee42a36ce542652b5f4b63d7254a64a
SaMiSaHibZaDa <786samiskust@gmail.com>
2026-05-02 08:20:17 +0500

    deleted the button..

--- src/App.jsx
+++ src/App.jsx
@@ -44,12 +44,6 @@ function App() {
           >
             Reset
           </button>
-          <button
-            className="  border-2 border-amber-700 bg-blue-500 rounded "
-            onClick={() => setNumber(number)}
-          >
-            Submit
-          </button>
 
           <button
             onClick={() => setDarkMode(!darkMode)}
